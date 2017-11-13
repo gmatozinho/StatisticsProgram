da0ca57c94bd4c34c4f09762d5689e324f93aaa0
Gustavo Matozinho Lima <gustavomatozinho@gmail.com>
2017-11-13 11:00:32 -0200

    add amplitude calculator e class calculator

--- StatisticsProgram/StatisticsProgram/MainPage.cs
+++ StatisticsProgram/StatisticsProgram/MainPage.cs
@@ -18,7 +18,7 @@ namespace StatisticsProgram
             Stream stream = assembly.GetManifestResourceStream("StatisticsProgram.teste.csv");
 
             string[] test;
-            using (var reader = new System.IO.StreamReader(stream))
+            using (var reader = new StreamReader(stream))
             {
                 test = reader.ReadToEnd().Split(new[] {"\r\n"}, StringSplitOptions.RemoveEmptyEntries);
             }
@@ -26,8 +26,13 @@ namespace StatisticsProgram
             var untreatyears = test[0];
             var data = test[1];
 
-            var values = data.Split(new[] {";"}, StringSplitOptions.None).ToList();
-            values.Remove("Brasil");
+            var originalList = data.Split(new[] {";"}, StringSplitOptions.None).ToList();
+            originalList.Remove("Brasil");
+
+            var doubleList = ConvertToListDouble(originalList);
+            var classNumber = ClassNumberCalculator(doubleList.Count());
+            doubleList.Sort();
+
 
             Content = new StackLayout
             {
@@ -35,56 +40,71 @@ namespace StatisticsProgram
                 VerticalOptions = LayoutOptions.Center,
                 Children = {
                     new Label { Text = "Valores Estatisticos:" },
-                    new Label { Text = "Média:" + CalculateMedia(values)},
-                    new Label { Text = "Moda:" + CalculateModa(values)},
-                    new Label { Text = "Mediana:" + CalculateMediana(values)},
+                    new Label { Text = "Média:" + CalculateMedia(doubleList)},
+                    new Label { Text = "Moda:" + CalculateModa(doubleList)},
+                    new Label { Text = "Mediana:" + CalculateMediana(doubleList)},
                     new Label { Text = data },
+                    new Label {Text = "Classes:" + classNumber},
+                    new Label {Text = "Amplitude:" + ClassAmplitudeCalculator(classNumber, doubleList[doubleList.Count-1],doubleList[0])}
                 }
             };
         }
 
-        private static string CalculateMediana(List<string> values)
+
+        private static List<double> ConvertToListDouble(List<string> values)
         {
-            var list = values.Select(double.Parse).ToList();
+            var doubleList = new List<double>();
+
+            foreach (var value in values)
+            {
+                doubleList.Add(double.Parse(value.Replace(",", ".")));
+            }
+
+            return doubleList;
+        }
+
+        private static string CalculateMediana(List<double> values)
+        {             
 
             string mediana;
 
-            if (list.Count % 2 == 0)
+            if (values.Count % 2 == 0)
             {
-                var medMe = list.Count / 2;
-                var medMa = (list.Count + 2) / 2;
-
-                // ReSharper disable once PossibleLossOfFraction
-                
-                mediana = " Elemento = " + ((list[medMa-1] + list[medMe-1])/2);
+                var medMe = values.Count / 2;
+                var medMa = (values.Count + 2) / 2;
+                                
+                mediana = " Elemento = " + ((values[medMa-1] + values[medMe-1])/2);
 
             }
             else
             {
-                // ReSharper disable once PossibleLossOfFraction
-                mediana = "Elemento = " + list[((list.Count + 1) / 2)-1];
+                mediana = "Elemento = " + values[((values.Count + 1) / 2)-1];
             }
 
             return mediana;
         }
 
-        private static double CalculateMedia(IReadOnlyCollection<string> values)
+        private static double CalculateMedia(List<double> values)
         {
-            var soma = values.Sum(value => double.Parse(value));
+            double soma = 0;
+
+            foreach(var value in values)
+            {
+                soma += value;
+            }
+
 
             return soma / values.Count;
         }
 
-        private static string CalculateModa(IEnumerable<string> values)
+        private static string CalculateModa(List<double> values)
         {
-            var list = values.Select(double.Parse).ToList();
-
-            string moda;
+                        string moda;
             
-            var groups = list.GroupBy(i => i).Select(i => new { Number = i.Key, Count = i.Count() }).OrderByDescending(i => i.Count);
+            var groups = values.GroupBy(i => i).Select(i => new { Number = i.Key, Count = i.Count() }).OrderByDescending(i => i.Count);
 
             var dict = new Dictionary<double, int>();
-            foreach (var d1 in list)
+            foreach (var d1 in values)
             {
                 var d = d1;
                 if (dict.ContainsKey(d))
@@ -93,7 +113,7 @@ namespace StatisticsProgram
                     dict.Add(d, 1);
             }
 
-            if (dict.Count == list.Count)
+            if (dict.Count == values.Count)
             {
                 moda = "Amodal";
             }
@@ -117,5 +137,27 @@ namespace StatisticsProgram
 
             return moda;
         }
+
+
+        private int ClassNumberCalculator(int qtdNumbers)
+        {
+            var classNumber = (int)Math.Round(Math.Sqrt(qtdNumbers));
+
+            return classNumber;
+        }
+
+
+        private int ClassAmplitudeCalculator(int classNumber, double maxValue, double minValue)
+        {
+            var classAmplitude = (maxValue - minValue) / classNumber;
+
+            return (int)Math.Round(classAmplitude);
+        }
+
+
+
+
+
+
     }
 }
